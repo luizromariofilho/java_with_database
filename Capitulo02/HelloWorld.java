d993f9e54e86a14c9e553491b0b98097367ede02
Luiz Romário Filho <luizromariofilho@gmail.com>
2016-02-10 21:43:29 -0300

    Initial commit
    First commit

---
+++ Capitulo02/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+* Imprimindo "Hello World!" no prompt de comando.
+*/
+public class HelloWorld {
+	// Metodo principal por onde inicia a execucao do programa
+	public static void main(String[] args) {
+		// Imprime texto "Hello World!" no prompt padrao
+		System.out.println("Hello World!");
+	}
+}
